53c4aa5efeb6c38d3182bf7fab322a9a5cdc1c48
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-11 21:55:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,17 +19,15 @@ export const app = express()
 app.use(express.json()) // to send data to db
 app.use(cookieParser())
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.header('Origin'));
   res.header("Access-Control-Allow-Credentials", true);
-    res.header(
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
-  
 });
 
 app.use(cors({ origin: true }));
