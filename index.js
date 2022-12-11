42985564b02561ddf630ea7e43699d46f82ae3b2
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-11 21:49:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(cookieParser())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  
 });
 
 app.use(cors({ origin: true }));
