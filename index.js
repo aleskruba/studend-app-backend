c556d58709f5192291195d9fa353a425d27ef650
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-11 21:53:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.header('Origin'));
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
   
 });
 
