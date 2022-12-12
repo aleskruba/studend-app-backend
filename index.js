3bf566b00b16e2011b55595fa0e887880a4da8e1
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-12 08:46:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,20 @@ export const app = express()
 app.use(express.json()) // to send data to db
 app.use(cookieParser())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.header('Origin'));
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+//  res.header("Access-Control-Allow-Credentials", true);
+//  res.header(
+//    "Access-Control-Allow-Headers",
+//    "Origin, X-Requested-With, Content-Type, Accept"
+//  );
+//  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//  next();
+// });
 
-app.use(cors({ origin: true }));
+app.use(cors({ 
+    origin: 'https://student-app-frontend.onrender.com' }));
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
