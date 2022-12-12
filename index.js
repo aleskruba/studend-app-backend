cccba910cf10f4f94b706c78322fa6684e6fef94
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-12 09:28:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ export const app = express()
 app.use(express.json()) // to send data to db
 app.use(cookieParser())
 
-// app.use(function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", req.header('Origin'));
-//  res.header("Access-Control-Allow-Credentials", true);
-//  res.header(
-//    "Access-Control-Allow-Headers",
-//    "Origin, X-Requested-With, Content-Type, Accept"
-//  );
-//  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//  next();
-// });
+ app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ''https://*.onrender.com' ');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+ });
 
 app.use(cors({ 
     origin: 'https://*.onrender.com' }));
