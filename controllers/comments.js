af61941927ef5723763cad15340b107a8d9a4f97
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-12 13:17:53 +0100

    Update comments.js

--- controllers/comments.js
+++ controllers/comments.js
@@ -25,11 +25,11 @@ export const getComment = (req,res) => {
 
 
 export const addComment = (req,res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
+//  const token = req.cookies.access_token;
+//  if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
+//  jwt.verify(token, "jwtkey", (err, userInfo) => {
+  //  if (err) return res.status(403).json("Token is not valid!");
 
     const q = "INSERT INTO comments (`title`,`comment`, `date`,`uid`) VALUES (?)"
     const values = [
@@ -43,7 +43,7 @@ export const addComment = (req,res) => {
       if (err) return res.status(500).json(err)
 
       return res.json("Post has been created")
-    })
+  //  })
 
    });
 }
