1030a1ba908fa4b989c1971c966dfb7fa2525b5a
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-12 17:48:17 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -4,20 +4,20 @@ import  {db } from "../db.js"
 export const users = (req,res) =>{
 
   
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
-
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
+ // const token = req.cookies.access_token;
+ // if (!token) return res.status(401).json("Not authenticated!");
+//
+ // jwt.verify(token, "jwtkey", (err, userInfo) => {
+ //   if (err) return res.status(403).json("Token is not valid!");
+//
     const q =   "SELECT * FROM users WHERE ID=?";
   
-    db.query(q,[userInfo.id],(err,data)=>{
+    db.query(q,[req.body.id],(err,data)=>{
         if(err) return res.send(err)
         return res.status(200).json(data)
     })
 
 
-  })
+ // })
 
 }
